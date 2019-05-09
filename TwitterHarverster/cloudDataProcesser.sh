a80b89d2e4ef510a4299c1b75835379a1fdf7b25
WY <707644624@qq.com>
2019-05-09 18:28:27 +1000

    docReader

--- TwitterHarverster/cloudDataProcesser.sh
+++ TwitterHarverster/cloudDataProcesser.sh
@@ -9,16 +9,19 @@ do
     for city in "${cities[@]}"
     do
         echo ${city}${year}.json
+        echo 'start_key=["${city}",${year},1,1]'
 
         curl "http://45.113.232.90/couchdbro/twitter/_design/twitter/_view/summary" \
         -G \
-        --data-urlencode 'start_key=["adelaide",2016,1,1]' \
-        --data-urlencode 'end_key=["adelaide",2017,1,31]' \
+        --data-urlencode 'start_key=["'${city}'",'${year}',1,1]' \
+        --data-urlencode 'end_key=["'${city}'",'${year}',1,31]' \
         --data-urlencode 'reduce=false' \
         --data-urlencode 'include_docs=true' \
         --user "readonly:ween7ighai9gahR6" \
         -o /data/${city}${year}.json
+
         echo start docReader ${city}${year}.json
+
         python3 docReader.py ${city}${year}.json 1>docout.txt 2>docerror.txt
 
         # nohup python3 docReader.py 1>docout.txt 2>docerror.txt &
@@ -27,4 +30,6 @@ do
     done
 
 done
+
+
 
