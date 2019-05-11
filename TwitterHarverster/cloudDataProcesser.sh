ec271866605985e39d27316d8b787fd8959e8d1d
WY <707644624@qq.com>
2019-05-12 00:47:20 +1000

    clean

--- TwitterHarverster/cloudDataProcesser.sh
+++ TwitterHarverster/cloudDataProcesser.sh
@@ -25,7 +25,8 @@ do
             -o /data/${city}${year}${month}.json
 
             echo start docReader ${city}${year}${month}.json
-            python3 docReader.py ${city}${year}${month}.json 1>docout.txt 2>docerror.txt
+            python3 docReader.py ${city}${year}${month}.json
+            #1>docout.txt 2>docerror.txt
 
             # nohup python3 docReader.py 1>docout.txt 2>docerror.txt &
             sudo rm -f /data/${city}${year}${month}.json
