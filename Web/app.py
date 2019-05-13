1d1692e8672b882c06b7a1128b2c3ae8044a42e5
kataraSSS <50225577+kataraSSS@users.noreply.github.com>
2019-05-13 18:14:17 +1000

    back-end

---
+++ Web/app.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import deal
+import json
+from tornado.options import define, options
+
+define("port", default=8888, help="run on the given port", type=int)
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.render('index.html')
+
+
+class ChartHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.render('chart.html')
+
+    def post(self):
+        command = self.get_argument('message')
+        if command == "chart":
+
+            income_drill = deal.income_drilldown()
+            map = {}
+            map["income_drill"] = income_drill
+
+            income_supercar = deal.income_supercar()
+            map["income_supercar"] = income_supercar
+            self.write(json.dumps(map))
+        if command == "update":
+            res = deal.update_tweet()
+            map={}
+            map["res"] = res
+            self.write(json.dumps(map))
+
+
+class MapHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.render('infotest.html')
+
+    def post(self):
+        region = self.get_argument('message')
+        print(region)
+        result = deal.pick_region(region)
+        map = {}
+        map["hour"] = result
+        brand_vader = deal.pick_brand(region)
+        map["brand"] = brand_vader
+        print(map)
+        self.write(json.dumps(map))
+
+
+
+settings = {
+    'template_path': 'template',
+    'static_path': 'static',
+    'static_url_prefix': '/static/',
+
+}
+
+application = tornado.web.Application([
+    (r"/", IndexHandler),
+    (r"/chart", ChartHandler),
+    (r"/map", MapHandler)
+], **settings)
+
+if __name__ == '__main__':
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
---
+++ Web/linkDB.py
@@ -0,0 +1,77 @@
+from cloudant.client import CouchDB
+import json
+
+USERNAME = 'admin'
+PASSWORD = 'admin'
+client = CouchDB(USERNAME,PASSWORD,url='http://172.26.38.69:5984',connect=True)
+session = client.session()
+
+def get_aurindata(db,file):
+
+    my_database = client[db]
+    my_document = my_database[file]
+
+    return my_document
+
+def get_supercar(dbname = 'car', ddocID = '_design/car' , viewID = 'superCar'):
+
+    doc = client[dbname].get_design_document(ddocID)
+    view = doc.get_view(viewID)
+    result = []
+    with view.custom_result(group_level=2) as rslt:
+        for doc in rslt.all():
+            #print(doc)
+            if doc["key"][1] is not None :
+                map={}
+                map["region"] = doc["key"][1]
+                map["vader"] = doc["value"]["sum"]/doc["value"]["count"]
+                result.append(map)
+
+    return result
+
+
+def get_count_score(dbname = 'twitter', ddocID = '_design/time' , viewID = 'score',region = ''):
+
+    doc = client[dbname].get_design_document(ddocID)
+    view = doc.get_view(viewID)
+    result = []
+    with view.custom_result(group_level=3) as rslt:
+        for doc in rslt.all():
+
+            if region == doc["key"][2]:
+                #print(doc)
+                map={}
+                map["time"] = doc["key"][0]
+                map["count"] = doc["value"]["count"]
+                map["score"] = doc["value"]["sum"]/doc["value"]["count"]
+                result.append(map)
+
+    return result
+
+def region_brand(dbname = 'car', ddocID = '_design/car' , viewID = 'byRegion', region = ''):
+    doc = client[dbname].get_design_document(ddocID)
+    view = doc.get_view(viewID)
+
+    brand_vader = []
+
+    with view.custom_result(group_level = 2) as rslt:
+        for doc in rslt.all():
+            #print(doc)
+            if region == doc["key"][0]:
+                map = {}
+                map["brand"] = doc["key"][1]
+                map["vader"] = doc["value"]["sum"]/doc["value"]["count"]
+                brand_vader.append(map)
+
+    return brand_vader
+
+def total_count(dbname = 'twitter', ddocID = '_design/time' , viewID = 'count'):
+    doc = client[dbname].get_design_document(ddocID)
+    view = doc.get_view(viewID)
+    count=0
+    with view.custom_result(group_level = 0) as rslt:
+        for doc in rslt.all():
+            print(doc)
+            return doc["value"]
+
+    return count
